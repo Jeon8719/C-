7c77e1232142e600b5da0625cb4e5ddbd3ac827f
Jeon8719 <98005218+Jeon8719@users.noreply.github.com>
2023-10-26 08:38:33 +0900

    Add files via upload

---
+++ Class1/DoWhile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class program
+{
+    static void Main(string[] args)
+    {
+        int a = 0;
+        do
+        {
+            Console.Write("숫자 입력 0=종료");
+            a = int.Parse(Console.ReadLine());
+        } while (a != 0); 
+        //입력 값이 0이 아니면 계속 반복함.
+        //while 조건식에 따라 do명령문을 반복하거나 중단
+    }
+}
