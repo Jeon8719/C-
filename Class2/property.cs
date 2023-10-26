b48a5a5751acb1318a44cdab8e5a7e0c7809e130
Jeon8719 <98005218+Jeon8719@users.noreply.github.com>
2023-10-26 16:35:36 +0900

    Add files via upload

---
+++ Class2/property.cs
@@ -0,0 +1,51 @@
+﻿class Person
+{
+    private string? name;
+    public string Name//property
+    {
+        get { return this.name; }
+        set { this.name = value; }
+    }
+    public void SetName(string? name)
+    {
+        this.name = name;
+    }
+    public string GetName()
+    {
+        return this.name;
+    }
+
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person();
+        person.SetName("손흥민");
+        string call = person.GetName();
+        Console.WriteLine(call);
+
+
+        Multi("A");
+        Multi("A", "B");
+        Multi("A", "B", "C");
+    }
+
+    static void Multi(params string[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (i == (arr.Length - 1)) Console.Write($"{arr[i]}");
+            else Console.Write($"{arr[i]}+");
+        }
+        Console.WriteLine();
+
+
+
+
+
+    }
+}
+
---
+++ Class2/reference.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data;
+
+class Program
+{
+    static void Do(ref int number)
+    {
+        number = 20;
+        Console.WriteLine(number);
+    }
+       
+    static void Main()
+    {
+        int number = 10;
+        Console.WriteLine(number);
+        Do(ref number);
+        //ref : reference 참조 받은 값으로 변경된 것이 유지된다.
+        Console.WriteLine(number);
+        Console.WriteLine($"현재시간:{DateTime.Now}");
+        DateTime dt;
+        if (DataSetDateTime.TryParse("2023-10-26", out dt))
+        {
+            Console.WriteLine(dt);
+        }
+        else
+        {
+            Console.WriteLine("날짜 형식으로 반환불가");
+        }
+    }
+}
+
+ 
+    
