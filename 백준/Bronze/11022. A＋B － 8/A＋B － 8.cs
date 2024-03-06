801e62e7e297a96668d6cb16f3a5d811ff1db1ac
Jeon Seong Woo <wjstjddn8719@naver.com>
2024-03-06 10:34:25 +0900

    [Bronze V] Title: A+B - 8, Time: 72 ms, Memory: 6216 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cs
@@ -0,0 +1,13 @@
+StreamReader sr = new(Console.OpenStandardInput());
+StreamWriter sw = new(Console.OpenStandardOutput());
+int a = int.Parse(sr.ReadLine());
+int result = 0;
+for (int i = 0; i < a; i++)
+{
+    int[] b = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+    result = b[0] + b[1];
+
+    sw.WriteLine($"Case #{i + 1}: {b[0]} + {b[1]} = {result}");
+}
+sw.Close();
+sr.Close();
