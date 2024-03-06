49c48cc2112711d8a93fb979ccbb3e28453fe040
Jeon Seong Woo <wjstjddn8719@naver.com>
2024-03-06 09:52:13 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 288 ms, Memory: 6600 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cs
@@ -0,0 +1,11 @@
+int A = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < A; i++)
+{
+    for (int j = 0; j < i+1; j++)
+    {
+        Console.Write("*");
+        
+    }
+    Console.WriteLine();
+
+}
