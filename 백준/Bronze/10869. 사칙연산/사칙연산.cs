6963e23ff7eaff1620200238974b6d2619a6f891
Jeon Seong Woo <wjstjddn8719@naver.com>
2024-03-06 10:32:18 +0900

    [Bronze V] Title: 사칙연산, Time: 64 ms, Memory: 5508 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cs
@@ -0,0 +1,6 @@
+int[] arry = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+Console.WriteLine(arry[0] + arry[1]);
+Console.WriteLine(arry[0] - arry[1]);
+Console.WriteLine(arry[0] * arry[1]);
+Console.WriteLine(arry[0] / arry[1]);
+Console.WriteLine(arry[0] % arry[1]);
